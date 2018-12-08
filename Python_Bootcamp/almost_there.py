d9133d92a804c266a74e71fd31c5fb87fd9b21b1
Arijit Basu <38849034+arijitbasu321@users.noreply.github.com>
2018-12-08 19:08:44 +0530

    Delete almost_there.py

--- Python_Bootcamp/almost_there.py
+++
@@ -1,9 +0,0 @@
-def almost_there(n):
-    if 90  <= n <= 110 or 190 <= n <= 210:
-        return  True
-    else:
-        return False
-
-user_input = int(input("Enter a number ").strip())
-
-print(almost_there(user_input))
