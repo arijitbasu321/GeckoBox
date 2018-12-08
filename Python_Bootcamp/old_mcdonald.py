f3374526a368d0974b7dffc8b4869584e2990d95
Arijit Basu <38849034+arijitbasu321@users.noreply.github.com>
2018-12-08 19:11:48 +0530

    Delete old_mcdonald.py

--- Python_Bootcamp/old_mcdonald.py
+++
@@ -1,8 +0,0 @@
-def old_macdonald(text):
-    text = text.capitalize()
-    return text[:3] + text[3].upper() + text[4:]
-
-
-user_input = input("enter you name: ")
-
-print(old_macdonald(user_input))
